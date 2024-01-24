7674e6473a1b2901d7e5bcac48f0e55f7b08c22e
Iqbal Hamdani <miqbalh94@gmail.com>
2024-01-24 23:08:17 +0700

    feat(tooltip): add ktv tooltip

---
+++ components/ktv/Tooltip.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+defineProps({
+  content: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger as-child><slot /></TooltipTrigger>
+      <TooltipContent>
+        <p>{{ content }}</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+</template>
