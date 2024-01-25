29f664ce14fc279c0c85fe7dfe43ff06e31a2711
Iqbal Hamdani <miqbalh94@gmail.com>
2024-01-25 20:15:22 +0700

    chore(config): remove runtime and setup auto-import component path and prefix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  runtimeConfig: {
-    // The private keys which are only available server-side
-    apiSecret: "123",
-    // Keys within public are also exposed client-side
-    public: {
-      apiBase: "/api",
-    },
-  },
-
   app: {
     pageTransition: { name: "page", mode: "out-in" },
   },
@@ -48,6 +39,14 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true,
   },
+
+  components: [
+    {
+      path: "~/components/ktv",
+      pathPrefix: false,
+      prefix: "ktv",
+    },
+  ],
 
   devtools: { enabled: true },
 });
