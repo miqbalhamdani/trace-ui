34fe68e408b8a193e7f31ff3ce7d09206418fbb6
IvanJK <77654312+ivanjk93@users.noreply.github.com>
2024-02-02 15:27:52 +0700

    feat(icon): test

---
+++ components/icon/Icon.vue
@@ -0,0 +1,42 @@
+<script setup>
+import * as TablerIcons from "@tabler/icons-vue";
+
+const props = defineProps({
+  icon: {
+    type: String,
+    default: "",
+  },
+  color: {
+    type: String,
+    default: "#121314",
+  },
+  size: {
+    type: String,
+    default: "large",
+  },
+});
+
+const iconComponents = Object.fromEntries(Object.entries(TablerIcons).map(([key, value]) => [key, ref(value)]));
+const getIconComponent = (iconName) => {
+  const iconRef = iconComponents[iconName];
+  if (iconRef) {
+    return iconRef.value;
+  } else {
+    return null;
+  }
+};
+const iconComponent = computed(() => getIconComponent(props.icon));
+const iconClasses = computed(() => `icon --${props.size}`);
+</script>
+
+<template>
+  <component
+    :is="iconComponent"
+    :color="color"
+    :class="iconClasses"
+  />
+</template>
+
+<style lang="scss" scoped>
+@import "./icon";
+</style>
---
+++ pages/test-icon/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+import KtvIcon from "~/components/icon/Icon.vue";
+</script>
+
+<template>
+  <div style="display: flex; justify-content: center; flex-direction: row; gap: 24px">
+    <KtvIcon
+      icon="IconHome"
+      size="small"
+    />
+    <KtvIcon
+      icon="IconHome"
+      size="medium"
+    />
+    <KtvIcon icon="IconHome" />
+    <KtvIcon
+      icon="IconHome"
+      size="extra-large"
+      color="#54a7dc"
+    />
+  </div>
+</template>
